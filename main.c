4b29833a3bfe8633ecde46a3aa05a2cee387c5f8
SMTix <SMTix@yandex.ru>
2016-03-04 13:29:22 +0300

    Add - !

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World\n");
-    printf("Goodbye, World\n");
+    printf("Hello, World!\n");
+    printf("Goodbye, World!\n");
     printf("New word's\n");
     return 0;
 }
